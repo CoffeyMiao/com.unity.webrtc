72fc3ceb76d8db124ab6d211e9f632c9855394ad
ryan <xuguang.cai@unity.cn>
2023-08-15 16:04:17 +0800

    fix

--- Runtime/Scripts/WebRTC.cs
+++ Runtime/Scripts/WebRTC.cs
@@ -1405,7 +1405,7 @@ namespace Unity.WebRTC
         [DllImport(WebRTC.Lib)]
         public static extern void AudioSourceProcessLocalAudio(IntPtr source, IntPtr array, int sampleRate, int channels, int frames);
         [DllImport(WebRTC.Lib)]
-        public static extern void AudioSourceProcessLocalAudioShort(Int16[] source, IntPtr array, int sampleRate, int channels, int frames);
+        public static extern void AudioSourceProcessLocalAudioShort(IntPtr source, Int16[] array, int sampleRate, int channels, int frames);
         [DllImport(WebRTC.Lib)]
         public static extern IntPtr StatsGetJson(IntPtr stats);
         [DllImport(WebRTC.Lib)]
